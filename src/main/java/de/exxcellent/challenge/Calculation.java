a24745e1cf7af4e858bc1e310df4bbe18b3e294d
Anja Schikorr <anja.schikorr@outlook.com>
2025-10-01 21:21:07 +0200

    Adding FootnallTask-class Recognizing both tasks have in general the same functionality - creating an interface whith methods which calculate the difference of values of two lists and getting the index of the smallest value of a list Until now no adjustment to "WeatherTask.java"

---
+++ src/main/java/de/exxcellent/challenge/Calculation.java
@@ -0,0 +1,47 @@
+package de.exxcellent.challenge;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public interface Calculation {
+
+
+    /**
+     * @param list1
+     * @param list2
+     * @return This method returns a integer list of the absolute difference of the values of list1 and list2
+     */
+    static List<Integer> CalculateAbsoluteDifferences(List<String> list1, List<String> list2){
+
+        List<Integer> differences = new ArrayList<>();
+
+        for (int i = 0; i < list1.size() - 1; i++) {
+            int difference = Math.abs(Integer.parseInt(list1.get(i)) - Integer.parseInt(list2.get(i)));
+            differences.add(difference);
+        }
+
+        return differences;
+
+    }
+
+    /**
+     * @param list
+     * @return This method returns the index of the smallest value in the given list
+     */
+    static int GetSmallestValueIndex(List<Integer> list){
+
+        int smallestDifference = list.get(0);
+        int indexOfSmallestDifference = -1;
+
+        for (int i = 0; i < list.size() - 1; i++) {
+            if (list.get(i) < smallestDifference) {
+                smallestDifference = list.get(i);
+                indexOfSmallestDifference = i;
+            }
+        }
+
+        return indexOfSmallestDifference;
+
+    }
+
+}
