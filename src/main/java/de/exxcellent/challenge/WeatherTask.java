002b43032a377ca1eb47fd4d17e7dcc42861e4f9
Anja Schikorr <anja.schikorr@outlook.com>
2025-10-01 21:27:22 +0200

    adjusting WeatherTask.java to Calculation interface

--- src/main/java/de/exxcellent/challenge/WeatherTask.java
+++ src/main/java/de/exxcellent/challenge/WeatherTask.java
@@ -5,44 +5,17 @@ import java.util.List;
 
 public class WeatherTask {
     private static final String pathWeatherFile = "ExxellentSolution-CodingChallenge\\src\\main\\resources\\de\\exxcellent\\challenge\\weather.csv";
-    private static  List<Integer> temperatureSpreads = new ArrayList<>();
 
     /**
-     * This function calculates the temperature spreads between the minimum
-     * and maximum temperature and writes the results into the list "temperatureSpreads"
-     */
-    private static void CalculateTemperatureSpreads() {
-        List<String> minTemp = new ArrayList<>(CSVParser.ReadCSVFileColumn(pathWeatherFile,"MnT"));
-        List<String> maxTemp = new ArrayList<>(CSVParser.ReadCSVFileColumn(pathWeatherFile,"MxT"));
-
-        for (int i = 0; i < minTemp.size() - 1; i++) {
-            int temperatureSpread = Integer.parseInt(maxTemp.get(i)) - Integer.parseInt(minTemp.get(i));
-            temperatureSpreads.add(temperatureSpread);
-        }
-    }
-
-
-    /**
-     * @return This function identifies the index of the smallest temperature spread
-     * of the list "temperatureSpread" and returns the day
+     * @return This function returns the day where the smallest temperature spread is calculated
      */
     public static String DayOfSmallestTemperatureSpread() {
+        List<String> minTemp = new ArrayList<>(CSVParser.ReadCSVFileColumn(pathWeatherFile,"MnT"));
+        List<String> maxTemp = new ArrayList<>(CSVParser.ReadCSVFileColumn(pathWeatherFile,"MxT"));
         List<String> dayList = new ArrayList<>(CSVParser.ReadCSVFileColumn(pathWeatherFile,"Day"));
 
-        CalculateTemperatureSpreads();
+        List<Integer> temperatureSpreads = new ArrayList<>(Calculation.CalculateAbsoluteDifferences(minTemp, maxTemp));
 
-        int smallestTemperatureSpread = temperatureSpreads.get(0);
-        int indexDaySmallesTempSpread = -1;
-
-        for (int i = 0; i < temperatureSpreads.size() - 1; i++) {
-            if (temperatureSpreads.get(i) < smallestTemperatureSpread) {
-                smallestTemperatureSpread = temperatureSpreads.get(i);
-                indexDaySmallesTempSpread = i;
-            }
-        }
-
-        return dayList.get(indexDaySmallesTempSpread);
+        return dayList.get(Calculation.GetSmallestValueIndex(temperatureSpreads));
     }
-
-
 }
